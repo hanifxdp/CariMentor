2f6febbf43bb156badfca1c7e3e476a4d627472c
hanifxdp <briandglory@gmail.com>
2021-05-11 21:17:50 +0700

    result indv page done

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from "@headlessui/react";
-import Link from "next/link";
 
 const navigation = [{ name: "About", href: "/about", current: true }];
 
@@ -10,16 +9,16 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-gray-100 shadow-md p-1">
+    <Disclosure as="nav" className="p-1 bg-gray-100 shadow-md">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="flex-1 flex items-center sm:items-stretch sm:justify-between">
-                <div className="flex-shrink-0 flex items-center">
+              <div className="flex items-center flex-1 sm:items-stretch sm:justify-between">
+                <div className="flex items-center flex-shrink-0" href="/">
                   {/* mobile */}
                   <img
-                    className="block lg:hidden h-8 w-auto"
+                    className="block w-auto h-8 lg:hidden"
                     src="/mobie.png"
                     alt="Workflow"
                   />
